f11271eed8c6c19eb66e7eb0f35026d06f254c5a
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-04-28 04:05:48 +0700

    📦 Initial commit

---
+++ include/erebus.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file erebus.hpp
+ * @author UnknownRori (68576836+UnknownRori@users.noreply.github.com)
+ * @brief Exposed API of project-erebus
+ * @version 0.1
+ * @date 2023-04-25
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#ifndef UNKNOWNRORI_PROJECT_EREBUS
+#define UNKNOWNRORI_PROJECT_EREBUS
+
+#endif
---
+++ src/erebus.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file erebus.cpp
+ * @author UnknownRori (68576836+UnknownRori@users.noreply.github.com)
+ * @brief Implementation details of project-erebus math resolver
+ * @version 1.0
+ * @date 2023-04-25
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include "../include/erebus.hpp"
